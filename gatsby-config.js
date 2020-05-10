db672cb54bd8dba7efa6b87eb442e5b1363aefa5
Ksmith30 <kylessmith@protonmail.com>
2020-05-10 12:08:14 -0500

    Add Offline Support for PWA enhancement

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/HBC.png`, // This path is relative to the root of the site.
+        icon: `src/images/HBC.png`,
       },
     },
-    `gatsby-plugin-styled-components`
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
   ],
 }
